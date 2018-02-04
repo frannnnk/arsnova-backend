a3524aadf14983512fcf99f3aa84fe6fb69cb6e7
Daniel Gerhardt <code@dgerhardt.net>
2018-02-04 08:17:03 +0100

    Create accounts on login for new guests

--- src/main/java/de/thm/arsnova/services/UserService.java
+++ src/main/java/de/thm/arsnova/services/UserService.java
@@ -67,4 +67,8 @@ public interface UserService {
 	void initiatePasswordReset(String username);
 
 	boolean resetPassword(UserProfile userProfile, String key, String password);
+
+	String createGuest();
+
+	boolean guestExists(String loginId);
 }
